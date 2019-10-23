bece0fb502c5aeeaef0fef9e4d07bdce1685ff03
Hajime Shiozawa <shiozawa@ripplex.com>
2019-10-23 17:10:07 +0900

    added chapter-1

---
+++ chapter-01/car-cdr.scm
@@ -0,0 +1,11 @@
+(car '(A B C))
+(car '(A))
+(car '())
+(car 'A)
+
+(cdr '(A B C))
+(cdr '(A))
+(cdr '())
+(cdr 'A)
+
+
---
+++ chapter-01/eq.scm
@@ -0,0 +1,6 @@
+(eq? 'atom 'atom)
+(eq? 1 2)
+(eq? '(1) '(1))
+(eq? '(atom) '(atom))
+(define a '(1))
+(eq? a a) ;
